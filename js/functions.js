0bfb1af194a24e5eb2a23bd36f704e62f97f63a5
Och-Erdene <ocherden@student.ubc.ca>
2026-04-13 23:54:21 -0700

    A bit touch up

--- js/functions.js
+++ js/functions.js
@@ -7,16 +7,11 @@ function toggleSidebar() {
     sidebar.classList.toggle('open');
 }
 
-function updateClock() {
-    var date = new Date();
-    document.getElementById("h3").innerHTML = date;
-}
-
 function generateStars(count) {
     let stars = '';
     for (let i = 0; i < count; i++) {
         const x = Math.floor(Math.random() * 2000);
-        const y = Math.floor(Math.random() * 2000);
+        const y = Math.floor(Math.random() * 1000);
         stars += `${x}px ${y}px #fff,`;
     }
     return stars.slice(0, -1);
